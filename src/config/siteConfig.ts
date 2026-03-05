0fb9ff7728717681746992131ed578bb2e985358
Ritsura <cht41286@gmail.com>
2026-03-06 01:05:42 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -7,10 +7,10 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "Ritsura",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "Personal site",
 
 	// 站点 URL
 	site_url: "https://firefly.cuteleaf.cn",
